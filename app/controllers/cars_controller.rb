24e5e6f2610b636196dc42f1d9b83c4efdea1e3d
Benjamin Fejzagic <benjamin.fejzagic@hotmail.com>
2020-11-17 16:46:12 +0100

    Footer and navbar edited, car new created, user can add car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -10,6 +10,15 @@ class CarsController < ApplicationController
   def new
     @car = Car.new
   end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
   
   private 
 
