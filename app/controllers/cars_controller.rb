dec220711ddc2f8d9214ffb40672a8ec8bb1fa76
Benjamin Fejzagic <benjamin.fejzagic@hotmail.com>
2020-11-19 14:32:24 +0100

    map and search added

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,12 +2,19 @@ class CarsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ] 
   before_action :find_index, only: %i[show destroy]
   def index
-    @cars = Car.all
-  end
+    if params[:query].present?
+      @cars = Car.where("model ILIKE ?", "%#{params[:query]}%")
+    else
+      @cars = Car.all
+    end
 
-  def your_cars
-    @user = current_user
-    @cars = @user.Car.all
+    @markers = @cars.geocoded.map do |car|
+      {
+        lat: car.latitude,
+        lng: car.longitude
+      }
+    end
+
 
   end
 
@@ -40,6 +47,6 @@ class CarsController < ApplicationController
   end
 
   def car_params
-    params.require(:car).permit(:model, :price, :fuel_type, :consumption, :category, :seat_number, :transmission, :photo)
+    params.require(:car).permit(:model, :price, :fuel_type, :consumption, :category, :seat_number, :transmission, :photo, :address)
   end
 end
